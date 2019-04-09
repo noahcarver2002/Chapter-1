7e63479875e0c49feb9346884bb03b44965f7fc5
noah <noahcarver21@yahoo.com>
2019-04-09 12:37:02 -0500

    Initial commit

---
+++ src/TableAndChairs.java
@@ -0,0 +1,24 @@
+public class TableAndChairs {
+// Author: Brosius
+    // Date 04/09/2019
+    //Project: Tables And Chairs
+
+    public static void main(string[] args) {
+        System.out.println("building some Tables and Chairs");
+        System.out.println("/n/n/n/n");
+
+        System.out.println("C");
+        System.out.println("H");
+        System.out.println("A");
+        System.out.println("I");
+        System.out.println("R")
+        System.out.println("CHAIRCHAIR    E");
+        System.out.println("c        C     G ");
+        System.out.println("H         H     L"):
+        System.out.println("A         A"      );
+        System.out.println("I         I"      );
+        System.out.println("R         R"      );
+    }
+
+
+}
